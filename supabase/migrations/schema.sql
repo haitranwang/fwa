2297f7c58a90d39cf3f77c70f847ed661430424e
Henry <haitranwang@gmail.com>
2025-07-02 17:29:08 +0700

    Create schema.sql

---
+++ supabase/migrations/schema.sql
@@ -0,0 +1,551 @@
+
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+
+CREATE SCHEMA IF NOT EXISTS "public";
+
+
+ALTER SCHEMA "public" OWNER TO "pg_database_owner";
+
+
+COMMENT ON SCHEMA "public" IS 'standard public schema';
+
+
+
+CREATE TYPE "public"."class_status" AS ENUM (
+    'Đang hoạt động',
+    'Đã kết thúc'
+);
+
+
+ALTER TYPE "public"."class_status" OWNER TO "postgres";
+
+
+CREATE TYPE "public"."course_level" AS ENUM (
+    'basic',
+    'intermediate',
+    'advance'
+);
+
+
+ALTER TYPE "public"."course_level" OWNER TO "postgres";
+
+
+CREATE TYPE "public"."course_status" AS ENUM (
+    'Đang mở',
+    'Đang bắt đầu',
+    'Kết thúc'
+);
+
+
+ALTER TYPE "public"."course_status" OWNER TO "postgres";
+
+
+CREATE TYPE "public"."trang_thai_bai_nop" AS ENUM (
+    'Chưa làm',
+    'Đang chờ chấm',
+    'Đã hoàn thành'
+);
+
+
+ALTER TYPE "public"."trang_thai_bai_nop" OWNER TO "postgres";
+
+
+CREATE TYPE "public"."user_role" AS ENUM (
+    'student',
+    'teacher',
+    'admin'
+);
+
+
+ALTER TYPE "public"."user_role" OWNER TO "postgres";
+
+
+CREATE OR REPLACE FUNCTION "public"."handle_new_user"() RETURNS "trigger"
+    LANGUAGE "plpgsql"
+    SET "search_path" TO 'public'
+    AS $$
+BEGIN
+    -- Function logic here
+    RETURN NEW;
+END;
+$$;
+
+
+ALTER FUNCTION "public"."handle_new_user"() OWNER TO "postgres";
+
+
+CREATE OR REPLACE FUNCTION "public"."update_updated_at_column"() RETURNS "trigger"
+    LANGUAGE "plpgsql"
+    SET "search_path" TO 'public'
+    AS $$
+BEGIN
+    NEW.updated_at := NOW();
+    RETURN NEW;
+END;
+$$;
+
+
+ALTER FUNCTION "public"."update_updated_at_column"() OWNER TO "postgres";
+
+SET default_tablespace = '';
+
+SET default_table_access_method = "heap";
+
+
+CREATE TABLE IF NOT EXISTS "public"."assignment_submissions" (
+    "id" "uuid" DEFAULT "extensions"."uuid_generate_v4"() NOT NULL,
+    "assignment_id" "uuid" NOT NULL,
+    "student_id" "uuid" NOT NULL,
+    "content" "jsonb",
+    "feedback" "text",
+    "status" "public"."trang_thai_bai_nop",
+    "submitted_at" timestamp with time zone,
+    "updated_at" timestamp with time zone DEFAULT "now"()
+);
+
+
+ALTER TABLE "public"."assignment_submissions" OWNER TO "postgres";
+
+
+CREATE TABLE IF NOT EXISTS "public"."assignments" (
+    "id" "uuid" DEFAULT "extensions"."uuid_generate_v4"() NOT NULL,
+    "lesson_id" "uuid" NOT NULL,
+    "instructor_id" "uuid" NOT NULL,
+    "content" "jsonb" NOT NULL,
+    "created_at" timestamp with time zone DEFAULT "now"(),
+    "updated_at" timestamp with time zone DEFAULT "now"()
+);
+
+
+ALTER TABLE "public"."assignments" OWNER TO "postgres";
+
+
+CREATE TABLE IF NOT EXISTS "public"."classes" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "course_id" "uuid" NOT NULL,
+    "name" "text" NOT NULL,
+    "description" "text",
+    "schedule" "text",
+    "status" "public"."class_status",
+    "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
+    "updated_at" timestamp with time zone DEFAULT "now"() NOT NULL,
+    "instructor_id" "uuid" NOT NULL
+);
+
+
+ALTER TABLE "public"."classes" OWNER TO "postgres";
+
+
+CREATE TABLE IF NOT EXISTS "public"."courses" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "name" "text" NOT NULL,
+    "description" "text",
+    "duration" integer NOT NULL,
+    "price" integer,
+    "image_url" "text",
+    "status" "public"."course_status" NOT NULL,
+    "created_at" timestamp with time zone DEFAULT "timezone"('utc'::"text", "now"()) NOT NULL,
+    "updated_at" timestamp with time zone DEFAULT "timezone"('utc'::"text", "now"()) NOT NULL,
+    "detail_lessons" "text",
+    "student_target" "text"
+);
+
+
+ALTER TABLE "public"."courses" OWNER TO "postgres";
+
+
+CREATE TABLE IF NOT EXISTS "public"."enrollments" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "student_id" "uuid" NOT NULL,
+    "class_id" "uuid" NOT NULL,
+    "enrolled_at" timestamp with time zone DEFAULT "now"() NOT NULL,
+    "status" "text" DEFAULT 'active'::"text",
+    CONSTRAINT "enrollments_status_check" CHECK (("status" = ANY (ARRAY['active'::"text", 'completed'::"text", 'dropped'::"text"])))
+);
+
+
+ALTER TABLE "public"."enrollments" OWNER TO "postgres";
+
+
+CREATE TABLE IF NOT EXISTS "public"."lessons" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "class_id" "uuid" NOT NULL,
+    "title" "text" NOT NULL,
+    "lesson_number" integer NOT NULL,
+    "content" "text",
+    "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
+    "updated_at" timestamp with time zone DEFAULT "now"() NOT NULL
+);
+
+
+ALTER TABLE "public"."lessons" OWNER TO "postgres";
+
+
+CREATE TABLE IF NOT EXISTS "public"."profiles" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "username" "text" NOT NULL,
+    "email" "text" NOT NULL,
+    "password" "text" NOT NULL,
+    "fullname" "text" NOT NULL,
+    "age" integer,
+    "phone_number" "text",
+    "role" "public"."user_role" DEFAULT 'student'::"public"."user_role",
+    "avatar_url" "text",
+    "created_at" timestamp with time zone DEFAULT "now"(),
+    "updated_at" timestamp with time zone DEFAULT "now"(),
+    "info" "text",
+    CONSTRAINT "profiles_age_check" CHECK ((("age" > 0) AND ("age" < 150)))
+);
+
+
+ALTER TABLE "public"."profiles" OWNER TO "postgres";
+
+
+ALTER TABLE ONLY "public"."assignment_submissions"
+    ADD CONSTRAINT "assignment_submissions_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."assignments"
+    ADD CONSTRAINT "assignments_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."classes"
+    ADD CONSTRAINT "classes_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."courses"
+    ADD CONSTRAINT "courses_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."enrollments"
+    ADD CONSTRAINT "enrollments_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."enrollments"
+    ADD CONSTRAINT "enrollments_student_id_course_id_key" UNIQUE ("student_id", "class_id");
+
+
+
+ALTER TABLE ONLY "public"."lessons"
+    ADD CONSTRAINT "lessons_course_id_lesson_number_key" UNIQUE ("class_id", "lesson_number");
+
+
+
+ALTER TABLE ONLY "public"."lessons"
+    ADD CONSTRAINT "lessons_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."profiles"
+    ADD CONSTRAINT "profiles_email_key" UNIQUE ("email");
+
+
+
+ALTER TABLE ONLY "public"."profiles"
+    ADD CONSTRAINT "profiles_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."profiles"
+    ADD CONSTRAINT "profiles_username_key" UNIQUE ("username");
+
+
+
+CREATE OR REPLACE TRIGGER "update_courses_updated_at" BEFORE UPDATE ON "public"."courses" FOR EACH ROW EXECUTE FUNCTION "public"."update_updated_at_column"();
+
+
+
+CREATE OR REPLACE TRIGGER "update_lessons_updated_at" BEFORE UPDATE ON "public"."lessons" FOR EACH ROW EXECUTE FUNCTION "public"."update_updated_at_column"();
+
+
+
+CREATE OR REPLACE TRIGGER "update_profiles_updated_at" BEFORE UPDATE ON "public"."profiles" FOR EACH ROW EXECUTE FUNCTION "public"."update_updated_at_column"();
+
+
+
+ALTER TABLE ONLY "public"."assignment_submissions"
+    ADD CONSTRAINT "assignment_submissions_assignment_id_fkey" FOREIGN KEY ("assignment_id") REFERENCES "public"."assignments"("id");
+
+
+
+ALTER TABLE ONLY "public"."assignment_submissions"
+    ADD CONSTRAINT "assignment_submissions_student_id_fkey" FOREIGN KEY ("student_id") REFERENCES "public"."profiles"("id");
+
+
+
+ALTER TABLE ONLY "public"."assignments"
+    ADD CONSTRAINT "assignments_instructor_id_fkey" FOREIGN KEY ("instructor_id") REFERENCES "public"."profiles"("id");
+
+
+
+ALTER TABLE ONLY "public"."assignments"
+    ADD CONSTRAINT "assignments_lesson_id_fkey" FOREIGN KEY ("lesson_id") REFERENCES "public"."lessons"("id");
+
+
+
+ALTER TABLE ONLY "public"."classes"
+    ADD CONSTRAINT "classes_course_id_fkey" FOREIGN KEY ("course_id") REFERENCES "public"."courses"("id");
+
+
+
+ALTER TABLE ONLY "public"."classes"
+    ADD CONSTRAINT "classes_instuctor_id_fkey" FOREIGN KEY ("instructor_id") REFERENCES "public"."profiles"("id");
+
+
+
+ALTER TABLE ONLY "public"."enrollments"
+    ADD CONSTRAINT "enrollments_class_id_fkey" FOREIGN KEY ("class_id") REFERENCES "public"."classes"("id");
+
+
+
+ALTER TABLE ONLY "public"."enrollments"
+    ADD CONSTRAINT "enrollments_student_id_fkey" FOREIGN KEY ("student_id") REFERENCES "public"."profiles"("id");
+
+
+
+ALTER TABLE ONLY "public"."lessons"
+    ADD CONSTRAINT "lessons_class_id_fkey" FOREIGN KEY ("class_id") REFERENCES "public"."classes"("id");
+
+
+
+CREATE POLICY "Cho phép tất cả người dùng cập nhật khóa học" ON "public"."courses" FOR UPDATE USING (true) WITH CHECK (true);
+
+
+
+CREATE POLICY "Cho phép tất cả người dùng tạo khóa học" ON "public"."courses" FOR INSERT WITH CHECK (true);
+
+
+
+CREATE POLICY "Cho phép tất cả người dùng xem khóa học" ON "public"."courses" FOR SELECT USING (true);
+
+
+
+CREATE POLICY "Cho phép tất cả người dùng xóa khóa học" ON "public"."courses" FOR DELETE USING (true);
+
+
+
+CREATE POLICY "Delete enrollments" ON "public"."enrollments" FOR DELETE USING (true);
+
+
+
+CREATE POLICY "Enable delete for authenticated users only" ON "public"."profiles" FOR DELETE USING ((( SELECT "auth"."uid"() AS "uid") IS NOT NULL));
+
+
+
+CREATE POLICY "Enable insert for all users" ON "public"."enrollments" FOR INSERT WITH CHECK (true);
+
+
+
+CREATE POLICY "Enable insert for authenticated users only" ON "public"."profiles" FOR INSERT WITH CHECK (("auth"."role"() = 'authenticated'::"text"));
+
+
+
+CREATE POLICY "Enable read access for all users" ON "public"."profiles" FOR SELECT USING (true);
+
+
+
+CREATE POLICY "Enable update for users based on email" ON "public"."profiles" FOR UPDATE USING (("auth"."role"() = 'authenticated'::"text"));
+
+
+
+CREATE POLICY "Enrolled students can view lessons" ON "public"."lessons" FOR SELECT USING (true);
+
+
+
+CREATE POLICY "Everyone can access profiles" ON "public"."profiles" USING (true) WITH CHECK (true);
+
+
+
+CREATE POLICY "Everyone can delete classes" ON "public"."classes" FOR DELETE USING (true);
+
+
+
+CREATE POLICY "Everyone can insert classes" ON "public"."classes" FOR INSERT WITH CHECK (true);
+
+
+
+CREATE POLICY "Everyone can select classes" ON "public"."classes" FOR SELECT USING (true);
+
+
+
+CREATE POLICY "Everyone can update classes" ON "public"."classes" FOR UPDATE USING (true);
+
+
+
+CREATE POLICY "Students can view their own enrollments" ON "public"."enrollments" FOR SELECT USING (true);
+
+
+
+CREATE POLICY "Teacher and admin can create lessons" ON "public"."lessons" FOR INSERT WITH CHECK (true);
+
+
+
+CREATE POLICY "Teacher and admin can delete lessons" ON "public"."lessons" FOR DELETE USING (true);
+
+
+
+CREATE POLICY "Teacher and admin can update lessons" ON "public"."lessons" FOR UPDATE USING (true);
+
+
+
+ALTER TABLE "public"."assignment_submissions" ENABLE ROW LEVEL SECURITY;
+
+
+CREATE POLICY "assignment_submissions_delete" ON "public"."assignment_submissions" FOR DELETE USING (true);
+
+
+
+CREATE POLICY "assignment_submissions_insert" ON "public"."assignment_submissions" FOR INSERT WITH CHECK (true);
+
+
+
+CREATE POLICY "assignment_submissions_select" ON "public"."assignment_submissions" FOR SELECT USING (true);
+
+
+
+CREATE POLICY "assignment_submissions_update" ON "public"."assignment_submissions" FOR UPDATE USING (true);
+
+
+
+ALTER TABLE "public"."assignments" ENABLE ROW LEVEL SECURITY;
+
+
+CREATE POLICY "assignments_delete" ON "public"."assignments" FOR DELETE USING (true);
+
+
+
+CREATE POLICY "assignments_insert" ON "public"."assignments" FOR INSERT WITH CHECK (true);
+
+
+
+CREATE POLICY "assignments_select" ON "public"."assignments" FOR SELECT USING (true);
+
+
+
+CREATE POLICY "assignments_update" ON "public"."assignments" FOR UPDATE USING (true);
+
+
+
+ALTER TABLE "public"."classes" ENABLE ROW LEVEL SECURITY;
+
+
+ALTER TABLE "public"."courses" ENABLE ROW LEVEL SECURITY;
+
+
+ALTER TABLE "public"."enrollments" ENABLE ROW LEVEL SECURITY;
+
+
+ALTER TABLE "public"."lessons" ENABLE ROW LEVEL SECURITY;
+
+
+ALTER TABLE "public"."profiles" ENABLE ROW LEVEL SECURITY;
+
+
+GRANT USAGE ON SCHEMA "public" TO "postgres";
+GRANT USAGE ON SCHEMA "public" TO "anon";
+GRANT USAGE ON SCHEMA "public" TO "authenticated";
+GRANT USAGE ON SCHEMA "public" TO "service_role";
+
+
+
+GRANT ALL ON FUNCTION "public"."handle_new_user"() TO "anon";
+GRANT ALL ON FUNCTION "public"."handle_new_user"() TO "authenticated";
+GRANT ALL ON FUNCTION "public"."handle_new_user"() TO "service_role";
+
+
+
+GRANT ALL ON FUNCTION "public"."update_updated_at_column"() TO "anon";
+GRANT ALL ON FUNCTION "public"."update_updated_at_column"() TO "authenticated";
+GRANT ALL ON FUNCTION "public"."update_updated_at_column"() TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."assignment_submissions" TO "anon";
+GRANT ALL ON TABLE "public"."assignment_submissions" TO "authenticated";
+GRANT ALL ON TABLE "public"."assignment_submissions" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."assignments" TO "anon";
+GRANT ALL ON TABLE "public"."assignments" TO "authenticated";
+GRANT ALL ON TABLE "public"."assignments" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."classes" TO "anon";
+GRANT ALL ON TABLE "public"."classes" TO "authenticated";
+GRANT ALL ON TABLE "public"."classes" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."courses" TO "anon";
+GRANT ALL ON TABLE "public"."courses" TO "authenticated";
+GRANT ALL ON TABLE "public"."courses" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."enrollments" TO "anon";
+GRANT ALL ON TABLE "public"."enrollments" TO "authenticated";
+GRANT ALL ON TABLE "public"."enrollments" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."lessons" TO "anon";
+GRANT ALL ON TABLE "public"."lessons" TO "authenticated";
+GRANT ALL ON TABLE "public"."lessons" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."profiles" TO "anon";
+GRANT ALL ON TABLE "public"."profiles" TO "authenticated";
+GRANT ALL ON TABLE "public"."profiles" TO "service_role";
+
+
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "service_role";
+
+
+
+
+
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "service_role";
+
+
+
+
+
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "service_role";
+
+
+
+
+
+
+RESET ALL;
